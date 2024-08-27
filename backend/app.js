0c52fa3aa5f7959de41403ad015815032d2cbc63
Hardip Chander <hardipchander@gmail.com>
2024-08-27 19:02:11 -0400

    chore(backend/app.js): node app connect with local mongodb server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express=require("express")
 const app=express();
+const mongoose=require("mongoose");
 
-app.listen(3000, ()=> {
-    console.log("Listening for requests on http://localhost:3000")
+app.get("/", (req, res) => {
+    console.log(req);
+    res.status(200).json({"Message": "Welcome to MERN Stack Tutorial"});
 });
+
+
+mongoose.connect("mongodb://localhost:27017/demo")
+.then(() => {
+    console.log("Connected to local MongoDB server");
+    app.listen(3000, ()=> {
+        console.log("Listening for requests on http://localhost:3000")
+    });
+}).catch(err => console.log(err))
